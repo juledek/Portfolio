de6f9028a9836d7e507571b55535ffb73c985731
juledek <jule.dekyvere@student.hogent.be>
2021-08-22 03:20:35 +0200

    changes in server.js

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/portfolio'));
 
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + `/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
-
+app.get('/*', function(req,res) {
+    
 res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
 });
 
